f0bb99d1781719629b66a6fa8015bcf61a4a3cbe
John McGuinness <j.mcguinness@icloud.com>
2020-10-17 19:46:59 +1100

    Added primitive parsers

--- src/main/java/com/github/johnmcguinness/fp/monparse/Parser.java
+++ src/main/java/com/github/johnmcguinness/fp/monparse/Parser.java
@@ -1,7 +1,32 @@
 package com.github.johnmcguinness.fp.monparse;
 
+import io.vavr.Tuple;
 import io.vavr.Tuple2;
+import io.vavr.collection.CharSeq;
 import java.util.List;
-import java.util.function.Function;
 
-public interface Parser<A> extends Function<String, List<Tuple2<A, String>>> { }
+@FunctionalInterface
+public interface Parser<A> { 
+
+	public static <A> Parser<A> result(A a) {
+		return input -> List.of(Tuple.of(a, input));
+	}
+
+	public static <A> Parser<A> zero() {
+		return input -> List.of();
+	}
+
+	public static Parser<Character> item() {
+		return input -> {
+			if(input == null || input.isEmpty()) {
+				return List.of();
+			}
+			else {
+				final CharSeq chars = CharSeq.of(input);
+				return List.of(Tuple.of(chars.head(), chars.tail().mkString()));
+			}
+		};
+	}
+	
+	public List<Tuple2<A, String>> parse(String input);
+}
